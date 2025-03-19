0671f44c09a5a1ac4ba8906c0375a6cfea4e0f14
brj01 <167606460+brj01@users.noreply.github.com>
2025-03-19 17:04:44 +0200

    add users

--- models/Article.js
+++ models/Article.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 
 const articleSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true }, // URL to article image
+  title:       { type: String, required: true },
+  image:       { type: String, required: true }, // URL to article image
   description: { type: String },                   // A short description or summary
-  date: { type: Date, required: true },
-  text: { type: String, required: true },   // Full article text
-  userID: { type: String, required: true },
-  minToRead: { type: Number, required: true },   // Estimated minutes to read
-  tag: { type: String, required: true },    // Tag or attribute for the article
-  pending: { type: Boolean, required: true, default: false }, // Default is false
-  comments: [{
-    userID: { type: String, required: true },
-    text: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-  }]
+  date:        { type: Date, required: true },
+  text:        { type:  String , required: true },   // Full article text
+  author:      { type: String, required: true },
+  minToRead:   { type: Number, required: true },   // Estimated minutes to read
+  accepted:    {type:Boolean,required:true}   // Tag or attribute for the article
 });
 
 module.exports = mongoose.model('Article', articleSchema);
