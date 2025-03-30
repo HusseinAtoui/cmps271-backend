19f108578093239a96c734b7c7377058bbd532f4
Hussein Atoui <husseinatoui20@gmail.com>
2025-03-30 15:24:50 +0300

    removing save feature

--- models/user.js
+++ models/user.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName:        { type: String, required: true },
-  lastName:         { type: String, required: true },
-  email:            { type: String, required: true, unique: true },
-  password:         { type: String, required: true },
-  bio:              { type: String, default: "" },
-  profilePicture:   { type: String, default: "" },
-  verificationToken:{ type: String },
-  verified:         { type: Boolean, default: false },
-  deactivated:      { type: Boolean, default: false },
-  savedArticles:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }] 
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bio: { type: String, default: "" },
+  profilePicture: { type: String, default: "" },
+  verificationToken: { type: String },
+  verified: { type: Boolean, default: false },
+  deactivated: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
